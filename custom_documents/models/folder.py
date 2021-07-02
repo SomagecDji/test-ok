f2c1d10375fe3ecea089e1b3354427c9d02e2094
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-07-02 16:30:51 +0100

    Update folder.py

--- custom_documents/models/folder.py
+++ custom_documents/models/folder.py
@@ -6,4 +6,11 @@ class DocumentFolder(models.Model):
     _inherit = 'documents.folder' 
     admin_group_ids = fields.Many2many('res.groups',  'documents_folder_admin_groups',string="Groupe d'écriture")
     active=fields.Boolean('Active', default=True)
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    def _domain_company(self):
+        company = self.env.company
+        return ['|', ('company_id', '=', False), ('company_id', '=', company)]
+    project_folder = fields.Many2one('documents.folder', string="Project Workspace", domain=_domain_company)
 
