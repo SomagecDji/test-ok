9b4f17254990165085c18ad90d3e1f22ffeb6ab0
SomagecDji <82517172+SomagecDji@users.noreply.github.com>
2021-07-16 19:15:36 +0100

    Update folder.py

--- custom_documents/models/folder.py
+++ custom_documents/models/folder.py
@@ -6,4 +6,18 @@ class DocumentFolder(models.Model):
     _inherit = 'documents.folder' 
     admin_group_ids = fields.Many2many('res.groups',  'documents_folder_admin_groups',string="Groupe d'écriture")
     active=fields.Boolean('Active', default=True)
+    def custom_groups(self):
+        folders=self.env['documents.folder'].search([])
+        for folder in folders:
+            if folder.parent_folder_id:
+                parent_folder_id=folder.parent_folder_id.id
+                #raise UserError(_(folder.group_ids))
+                parent_folder=self.env['documents.folder'].search([('id','=',parent_folder_id)])
+                if folder.read_group_ids:
+                    for id in [group.id for group in  folder.read_group_ids ]:
+                        parent_folder.write({'read_group_ids':[(4,id)]})
+                if folder.group_ids:
+                    for id in [group.id for group in  folder.group_ids ]:
+                        parent_folder.write({'read_group_ids':[(4,id)]})
+        return()
 
